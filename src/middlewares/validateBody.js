e54148d010f33a2abe4309f4c793d869adc42a38
Olena Piatkivska <157681009+olenapiatkivska@users.noreply.github.com>
2024-10-19 18:50:38 +0300

    update validateBody

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -2,7 +2,7 @@ import createHttpError from 'http-errors';
 
 export const validateBody = (schema) => async (req, res, next) => {
   try {
-    await schema.validateAsynk(req.body, {
+    await schema.validateAsync(req.body, {
       abortEarly: false,
     });
     next();
