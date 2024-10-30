6bfad1b9cbbbe7a925f4c853c2509a2b66edada1
Olena Piatkivska <157681009+olenapiatkivska@users.noreply.github.com>
2024-10-30 20:59:56 +0200

    hw6

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,13 @@
 import createHttpError from 'http-errors';
+import jwt from 'jsonwebtoken';
 import { randomBytes } from 'crypto';
 import { UsersCollection } from '../db/models/user.js';
 import bcrypt from 'bcrypt';
 import { FIFTEEN_MINUTES, THIRTY_DAYS } from '../constants/index.js';
 import { SessionsCollection } from '../db/models/session.js';
+import { env } from '../utils/env.js';
+import { SMTP } from '../constants/index.js';
+import { sendEmail } from '../utils/sendMail.js';
 
 export const registerUser = async (payload) => {
   const user = await UsersCollection.findOne({ email: payload.email });
@@ -82,5 +86,32 @@ export const refreshUsersSession = async ({ sessionId, refreshToken }) => {
   return await SessionsCollection.create({
     userId: session.userId,
     ...newSession,
+  });
+};
+
+export const requestResetToken = async (email) => {
+  const user = await UsersCollection.findOne({ email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  // console.log(user);
+
+  const resetToken = jwt.sign(
+    {
+      sub: user._id,
+      email,
+      // email: user.email,
+    },
+    env('JWT_SECRET'),
+    {
+      expiresIn: '15m',
+    },
+  );
+
+  await sendEmail({
+    from: env(SMTP.SMTP_FROM),
+    to: email,
+    subject: 'Reset your password',
+    html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
   });
 };
